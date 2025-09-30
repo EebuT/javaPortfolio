35990cf0c303d6975768846a44828333c6fe2134
EebuT <eetuatuominen@gmail.com>
2025-09-30 11:03:16 +0300

    variablesInJava creation

---
+++ src/variablesInJava.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class variablesInJava {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please insert the following information: ");
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Age: ");
+        int age = sc.nextInt();
+        System.out.println("Height(in meters eg. 1,8 = 180cm): ");
+        double height = sc.nextDouble();
+        System.out.printf("Your information: %nName: " + name + "%nAge: " + age + "%nHeight: "+ height);
+        sc.close();
+    }
+}
