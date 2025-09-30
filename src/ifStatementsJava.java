1d6da03233d2e97c4d524d03c2a45731b39f5c22
EebuT <eetuatuominen@gmail.com>
2025-09-30 13:00:32 +0300

    ifStatementsInJava creation

---
+++ src/ifStatementsJava.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ifStatementsJava {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please insert an integer number between -100 and 100: ");
+    int num = sc.nextInt();
+    if (num<-100 || num >100){
+        System.out.println("Given number was not in range. The program will shutdown.");
+        System.exit(0);
+    }
+
+    Boolean isEven =(num%2==0);
+    Boolean isPositive =(num>0);
+    Boolean isZero =(num==0);
+
+
+    if (isZero){ 
+    }
+    else if(isEven){
+        System.out.println("The number is even"); 
+     } else if (!isEven) {
+        System.out.println("The number is uneven"); 
+     } 
+
+     if (isZero) {
+        System.out.println("The number is zero"); 
+     }
+     else if(isPositive) {
+        System.out.println("The number is positive"); 
+
+     } else if (!isPositive){
+        System.out.println("The number is negative"); 
+     }
+    
+    sc.close(); 
+    }
+}
